259c747e170a6e8361b76994bf1a384b0a1dcf76
Bui Duc Manh <manh6947@gmail.com>
2022-07-05 14:19:27 +0700

    add html, css to view rails

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,2 +1,33 @@
 class UsersController < ApplicationController
+  def index
+    @users = User.all
+  end
+
+  def show
+    @user = User.find(params[:id])
+  rescue StandardError => e
+    @errors = e
+    @users = User.all
+    flash.now[:alert] = 'This user was not found'
+    render 'index'
+  end
+
+  def edit
+    @user = User.find(params[:id])
+  end
+
+  def update
+    @user = User.find(params[:id])
+    if @user.update(params.require(:user).permit(:name, :phone_number, :images, :avatar))
+      redirect_to edit_user_path(@user)
+    else
+      flash.now[:errors] = 'This is an error'
+      render :edit, status: :unprocessable_entity
+    end
+  end
+
+  def destroy
+    @user = User.find(params[:id])
+    redirect_to :show if @user.destroy
+  end
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,2 +1,5 @@
 class User < ApplicationRecord
+  validates :name, presence: true
+  has_many_attached :images
+  has_one_attached :avatar
 end
