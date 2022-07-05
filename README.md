ff0186f020b3525648a451feb69bb84b7e5dbfc8
Bui Duc Manh <manh6947@gmail.com>
2022-07-05 17:08:57 +0700

    add to importmap

--- README.md
+++ README.md
@@ -2,5 +2,14 @@
 
 ## Start
 
-Run in terminal: rails server
+Step to run
+
+### Step 1:
+Run: bundle install
+
+### Step 2:
+Run: yarn
+
+### Step 2
+Run: rails server
 
--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,10 +1,24 @@
 class UsersController < ApplicationController
+  before_action :find_user, only: %i[show edit update destroy]
+
   def index
     @users = User.all
   end
 
+  def new
+    @user = User.new
+  end
+
+  def create
+    @user = User.new(user_params)
+    if @user.save
+      redirect_to users_path
+    else
+      render :new, status: :unprocessable_entity
+    end
+  end
+
   def show
-    @user = User.find(params[:id])
   rescue StandardError => e
     @errors = e
     @users = User.all
@@ -12,13 +26,10 @@ class UsersController < ApplicationController
     render 'index'
   end
 
-  def edit
-    @user = User.find(params[:id])
-  end
+  def edit; end
 
   def update
-    @user = User.find(params[:id])
-    if @user.update(params.require(:user).permit(:name, :phone_number, :images, :avatar))
+    if @user.update(user_params)
       redirect_to edit_user_path(@user)
     else
       flash.now[:errors] = 'This is an error'
@@ -27,7 +38,16 @@ class UsersController < ApplicationController
   end
 
   def destroy
+    redirect_to users_path if @user.destroy
+  end
+
+  private
+
+  def find_user
     @user = User.find(params[:id])
-    redirect_to :show if @user.destroy
+  end
+
+  def user_params
+    params.require(:user).permit(:id, :name, :phone_number, :images, :avatar, :address)
   end
 end
